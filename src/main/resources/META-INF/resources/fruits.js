aacd4c0c754f238ab60f2f4a822ff5c4b7ca7195
Leandro <lescoutinhovr@gmail.com>
2020-08-27 10:11:56 -0300

    Finishes update logic

--- src/main/resources/META-INF/resources/fruits.js
+++ src/main/resources/META-INF/resources/fruits.js
@@ -2,16 +2,18 @@ function refresh() {
     $.get('/fruits', function (fruits) {
         var list = '';
         (fruits || []).forEach(function (fruit) { 
-            list = list
-                + '<tr>'
-                + '<td>' + fruit.id + '</td>'
-                + '<td><input type="text" id="updated-name" value="' + fruit.name + '"></td>'
-                + '<td><a href="#" onclick="deleteFruit(' + fruit.id + ')">Delete</a></td>'
-                + '</tr>'
+            list += ` 
+                <tr>
+                  <td>${fruit.id}</td>
+                  <td><input type="text" id="updated-name${fruit.id}" value="${fruit.name}"></td>
+                  <td><a href="#" onclick="deleteFruit(${fruit.id})">Delete</a></td>
+                  <td><a href="#" onclick="updateFruit(${fruit.id})">Update</a></td>
+                </tr>
+            `;
         });
         if (list.length > 0) {
             list = ''
-                + '<table><thead><th>Id</th><th>Name</th><th></th></thead>'
+                + '<table><thead><th>Id</th><th>Name</th><th></th><th></th></thead>'
                 + list
                 + '</table>';
         } else {
@@ -23,6 +25,12 @@ function refresh() {
 
 function deleteFruit(id) {
     $.ajax('/fruits/' + id, {method: 'DELETE'}).then(refresh);
+}
+
+function updateFruit(id) {
+    var fruitName = $(`#updated-name${id}`).val();
+    postData('/fruits', {id, name: fruitName})
+    .then(refresh);
 }
 
 $(document).ready(function () {
@@ -38,3 +46,22 @@ $(document).ready(function () {
 
     refresh();
 });
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
